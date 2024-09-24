ea38bdff33eb1327ad6e42ef5bb9c4eafc1d1b46
piyakit <piyakit.tho@gmail.com>
2024-09-24 15:23:49 +0700

    Fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-'use client'
+
 
 import { cookies } from "next/headers";
 import Image from "next/image";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(()=>{
     cookies().set('name', 'TestName')
     cookies().set('user', 'TestUser')
     cookies().set('ipAddress', '192.168.1.2')
-  }, [])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
